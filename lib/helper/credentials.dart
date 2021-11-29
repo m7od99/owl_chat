cb3f2cfdf3eb9fb0ac25d359fa631856fa70ccd2
Cal <m7od99@hotmail.com>
2021-11-29 17:36:52 +0300

    before merge commit

---
+++ lib/helper/credentials.dart
@@ -0,0 +1,7 @@
+// ignore: constant_identifier_names
+const FCM_SERVER_KEY =
+    '	AAAAQVqYIc8:APA91bEzZfW_SLLpKYUeKciVCtblL3dRssIJzWs9jbPJBpxngFwSE3aYVFusqi2GEvDX6PT6HIN0p4_Ye9DKmpKcuCiE7kZK55ihHDL3K-AJot2AjgpRK951dfafeEz2oDV15EN3-SIN';
+
+const imgurClintId = 'f21d31b59cacd81';
+
+const imgurClientSecret = '4fbee10678beeda1c431665e2a126a374b249971';
